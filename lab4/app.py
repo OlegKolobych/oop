ccfb6c6bf8928a1ec158df1b2d39aa2fd594b68b
oleg <okolobych2020@itcollege.lviv.ua>
2022-11-16 16:00:24 +0200

    kkk

---
+++ lab4/app.py
@@ -0,0 +1,14 @@
+import requests
+
+response = requests.get('https://httpbin.org/')
+for line in response.iter_lines():
+    print(line)
+
+from django.urls import include, path
+
+urlpatterns = [
+    # Assuming we're at the root, this will make the webhook
+    # available at /stripe/webhook/
+    path("stripe/", include("django_stripe.urls", namespace="stripe"))
+]
+STRIPE_WEBHOOK_SECRET = "whsec_0DoBceBjS0jjm7aQj459FXiFSluJEBxt"
